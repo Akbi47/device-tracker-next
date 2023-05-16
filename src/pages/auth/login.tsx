0876dbe29327513fdac32270c4be62f64f887d7b
Akbi47 <anhkhoaquachvo@gmail.com>
2023-05-16 21:52:32 +0700

    middleware for login feature

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,6 @@
 import { Box, Container, Grid, styled } from "@mui/material";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import { getSession } from "next-auth/react";
-import { useEffect } from "react";
 
 import { Logo } from "../../components/atoms";
 import { Base } from "../../components/layouts";
@@ -16,63 +14,62 @@ const MdGrid = styled(Grid)(({ theme }) => ({
   },
 }));
 
-const LoginPage: NextPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkSession = async () => {
-      const session = await getSession();
-
-      // If the user's session exists, redirect them to another page
-      if (session) {
-        router.replace("http://localhost:3000/tracker");
-      }
-    };
-
-    checkSession();
-  }, [router]);
-
-  return (
-    <Base title="Login">
-      <Box
-        sx={{
-          height: "100%",
-          minHeight: "260px",
-          display: "grid",
-          justifyContent: "center",
-          alignItems: "center",
-          gridAutoFlow: "column",
-        }}
-      >
-        <Container maxWidth="md">
-          <Grid container spacing={2}>
-            <Grid
-              item
-              xs={12}
-              md={6}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Logo size="largex" />
-            </Grid>
-            <Grid item xs={12} md={6} />
-            <MdGrid item xs={12} md={6}>
-              <AuthInfo />
-            </MdGrid>
-            <Grid item xs={12} md={6}>
-              <LoginForm />
-            </Grid>
-            <Grid item xs={12}>
-              <AuthFooter />
-            </Grid>
+const LoginPage: NextPage = () => (
+  <Base title="Login">
+    <Box
+      sx={{
+        height: "100%",
+        minHeight: "260px",
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+        gridAutoFlow: "column",
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Logo size="largex" />
           </Grid>
-        </Container>
-      </Box>
-    </Base>
-  );
-};
+          <Grid item xs={12} md={6} />
+          <MdGrid item xs={12} md={6}>
+            <AuthInfo />
+          </MdGrid>
+          <Grid item xs={12} md={6}>
+            <LoginForm />
+          </Grid>
+          <Grid item xs={12}>
+            <AuthFooter />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </Base>
+);
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
 
+  if (session) {
+    // User is logged in, redirect to the dashboard or home page
+    return {
+      redirect: {
+        destination: "/dashboard", // Replace with your desired page
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {}, // Proceed with rendering the login page
+  };
+}
 export default LoginPage;
